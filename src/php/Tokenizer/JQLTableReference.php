69f9cdcdebc4928a5045c2aa3f115b423e8be3a6
Andrei Sfia <sfia.andreidaniel@gmail.com>
2018-01-19 19:27:28 +0200

    Port TableReference

--- src/php/Tokenizer/JQLTableReference.php
+++ src/php/Tokenizer/JQLTableReference.php
@@ -2,7 +2,33 @@
 
 namespace JQL\Tokenizer;
 
-class JQLTableReference
+class JQLTableReference extends JQLOpcode
 {
+
+    /**
+     * @var string
+     */
+    public $name;
+
+    public function __construct(array $token)
+    {
+        $this->name = $token['name'];
+    }
+
+    /**
+     * @return string
+     */
+    public function getOpcodeType()
+    {
+        return EJQLLexerOpcodeTypes::TABLE;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
 
 }
