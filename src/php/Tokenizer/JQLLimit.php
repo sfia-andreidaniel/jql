c791a4224a61d27017b0698cb909adb7f9249e99
Andrei Sfia <sfia.andreidaniel@gmail.com>
2018-01-19 19:24:23 +0200

    Port JQLLimit

--- src/php/Tokenizer/JQLLimit.php
+++ src/php/Tokenizer/JQLLimit.php
@@ -3,7 +3,35 @@
 namespace JQL\Tokenizer;
 
 
-class JQLLimit
+class JQLLimit extends JQLOpcode
 {
 
+    /**
+     * @var int
+     */
+    private $limit;
+
+    /**
+     * @var int
+     */
+    private $skip;
+
+    /**
+     * JQLLimit constructor.
+     *
+     * @param array $token
+     */
+    public function __construct(array $token)
+    {
+        $this->limit = $token['limit'];
+        $this->skip = $token['skip'];
+    }
+
+    /**
+     * @return string
+     */
+    public function getOpcodeType()
+    {
+        return EJQLLexerOpcodeTypes::LIMIT_OPTION;
+    }
 }
