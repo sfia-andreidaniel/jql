bb6c9c632ad3f75e7ace58f457b735ad0a75c090
Andrei Sfia <sfia.andreidaniel@gmail.com>
2018-02-08 22:33:31 +0200

    Add backendToBackend secret in config

--- src/config/config.php
+++ src/config/config.php
@@ -5,6 +5,7 @@ return [
     'token.jti'                 => '4f1g23a12aa',
     'token.password'            => 'LKSJDO(#sdklo*quw#eksd>amkuy@#($*ylasdalkj(*(@#$',
     'token.ttl'                 => 86400,
+    'token.b2bSecret'           => '123456',
     'server.name'               => $_SERVER['SERVER_NAME'],
     'database.default.url'      => 'mysql:host=127.0.0.1;dbname=jql;charset=utf8mb4',
     'database.default.user'     => 'root',
